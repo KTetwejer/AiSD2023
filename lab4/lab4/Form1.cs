b2ebf0602c14521adcc573886b8216285cbeacb2
KTetwejer <115549398+KTetwejer@users.noreply.github.com>
2023-12-04 11:15:18 +0100

    main

--- lab4/lab4/Form1.cs
+++ lab4/lab4/Form1.cs
@@ -9,23 +9,23 @@ namespace lab4
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var w1 = new Wêze³(5);
-            var w2 = new Wêze³(1);
-            var w3 = new Wêze³(2);
+            var w1 = new Wezel(5);
+            var w2 = new Wezel(1);
+            var w3 = new Wezel(2);
             w1.dzieci.Add(w2);
             w1.dzieci.Add(w3);
-            var w4 = new Wêze³(3);
-            var w5 = new Wêze³(8);
+            var w4 = new Wezel(3);
+            var w5 = new Wezel(8);
             w2.dzieci.Add(w4);
             w2.dzieci.Add(w5);
-            var w6 = new Wêze³(4);
+            var w6 = new Wezel(4);
             w3.dzieci.Add(w6);
 
             A(w1);
         }
-        void A(Wêze³ w)
+        void A(Wezel w)
         {
-            MessageBox.Show(w.wartoœæ.ToString());
+            MessageBox.Show(w.wartosc.ToString());
             foreach(var dziecko in w.dzieci)
             {
                 A(dziecko);
---
+++ lab4/lab4/wezel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    internal class Wezel
+    {
+        public int wartosc;
+        public List<Wezel> dzieci = new();
+
+        public Wezel(int liczba)
+        {
+            this.wartosc = liczba;
+        }
+    }
+}
